11446a4e4a56706157326e0074c99ff6949f052f
LujanSolo <lujansolo@proton.me>
2022-08-23 14:47:00 -0700

    added and organized js variables; minor css corrections;psuedocoding cont

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,7 @@
 // possible variables to get by ID 
 var start = document.getElementById("start");
-var quiz = document.getElementById("quizBox");
-var question = document.getElementById("question");
-var userInitials = document.querySelector("#userInitials");
+var quiz = document.getElementById("quiz");
+var results = document.getElementById("results");
 
 var answer1 = document.getElementById("A");
 var answer2 = document.getElementById("B");
@@ -10,6 +9,9 @@ var answer3 = document.getElementById("C");
 var answer4 = document.getElementById("D");
 
 var timer = document.getElementById("timer");
+
+//possible querySelectors:
+var userInitials = document.querySelector("#userInitials");
 
 // an array for the questions
 var questions = [
