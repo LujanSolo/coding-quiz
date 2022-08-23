d3ac650f2e4aaaf1ac02503a2a92fcea1940893d
LujanSolo <lujansolo@proton.me>
2022-08-23 14:30:56 -0700

    replaced <section>s with <div>s; rebuilt results <div>

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,6 +2,7 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quizBox");
 var question = document.getElementById("question");
+var userInitials = document.querySelector("#userInitials");
 
 var answer1 = document.getElementById("A");
 var answer2 = document.getElementById("B");
@@ -9,3 +10,20 @@ var answer3 = document.getElementById("C");
 var answer4 = document.getElementById("D");
 
 var timer = document.getElementById("timer");
+
+// an array for the questions
+var questions = [
+  "Commonly used data types do NOT include:",
+  "The condition in an if / else statement is enclosed in __________.",
+  "Arrays in JavaScript can be used to store __________.",
+  "A very useful tool used during development and debugging for printing content to the debugger is:",
+  "String values must be enclosed within ___________ when being assigned to variables."
+];
+
+
+
+
+
+
+
+
