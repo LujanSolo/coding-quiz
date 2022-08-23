21851ffd7852caa2852a80d6ba58bd37ee7bd82b
LujanSolo <lujansolo@proton.me>
2022-08-22 23:27:10 -0700

    established starting variables in JS. Set display to none on quizbox/answers

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+// possible variables to get by ID 
+var start = document.getElementById("start");
+var quiz = document.getElementById("quizBox");
+var question = document.getElementById("question");
+
+var answer1 = document.getElementById("A");
+var answer2 = document.getElementById("B");
+var answer3 = document.getElementById("C");
+var answer4 = document.getElementById("D");
+
+var timer = document.getElementById("timer");
