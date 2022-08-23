06a68f0b7288ae47dfd72b4abd7dc615d0174b94
LujanSolo <lujansolo@proton.me>
2022-08-23 15:09:59 -0700

    buildout of initial functions; all docs edited for id matching

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,11 +2,12 @@
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
 var results = document.getElementById("results");
+var submitButton= document.getElementById("submitButton");
 
-var answer1 = document.getElementById("A");
-var answer2 = document.getElementById("B");
-var answer3 = document.getElementById("C");
-var answer4 = document.getElementById("D");
+var answerA = document.getElementById("A");
+var answerB = document.getElementById("B");
+var answerC = document.getElementById("C");
+var answerD = document.getElementById("D");
 
 var timer = document.getElementById("timer");
 
@@ -21,6 +22,15 @@ var questions = [
   "A very useful tool used during development and debugging for printing content to the debugger is:",
   "String values must be enclosed within ___________ when being assigned to variables."
 ];
+
+//POSSIBLE FUNCTIONS
+
+function startGame(){}
+
+function showResults(){}
+
+// submit button to go to results screen
+submitButton.addEventListener('click', showResults);
 
 
 
